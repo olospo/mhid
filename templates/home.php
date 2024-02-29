d173fc4a7feb6128016775214eeb193d7b2aaafc
Tom Brooks <tom@olospo.co.uk>
2024-02-29 13:06:33 +0000

    Home template changes

--- templates/home.php
+++ templates/home.php
@@ -9,71 +9,38 @@ $video = get_field('section_one_video');
 
 while ( have_posts() ) : the_post(); ?>
 
-<?php get_template_part( 'inc/hero' ); ?>
-
-<section class="post">
-  <div class="container flex">
-    <div class="content twelve columns">
-    <?php if (have_rows('section_content')) { // Flexible Content ?>
-    <div class="flexible_content">
-      <?php while (have_rows('section_content')) { the_row(); ?>
-        <?php if( get_row_layout() == 'columns' ): ?>
-          <?php get_template_part( 'flexible/columns'); // Column ?>
-        <?php elseif( get_row_layout() == 'content_block' ): ?>
-          <?php get_template_part( 'flexible/content_block'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'two_columns' ): ?>
-          <?php get_template_part( 'flexible/two_columns'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'columns' ): ?>
-          <?php get_template_part( 'flexible/columns'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'accordion' ): ?>
-          <?php get_template_part( 'flexible/accordion'); // Accordion Block ?>
-        <?php elseif( get_row_layout() == 'glossary' ): ?>
-          <?php get_template_part( 'flexible/glossary'); // Glossary ?>
-        <?php endif; ?>
-      <?php } ?>
-    </div>
-    <?php } // End Flexible Content?>  
-    </div>
-  </div>
-</section>
-
-<section class="home_carousel">
-  <div class="container">
-    <div class="heading twelve columns">
-      <h3><?php the_field('section_heading'); ?></h3>
-      <?php the_field('section_sub-heading'); ?>
-    </div>
-      
-      <?php if( have_rows('resource__post') ): ?>
-      <div class="slider twelve columns">
-      <?php while ( have_rows('resource__post')) : the_row(); // loop through the repeater fields ?>
-        <?php // set up post object
-          $post_object = get_sub_field('resource');
-          if( $post_object ) :
-          $post = $post_object;
-          setup_postdata($post);
-        ?>
-      
-        <article class="slide"> 
-          <a href="<?php the_permalink(); ?>"><img src="<?php the_post_thumbnail_url( 'background-img' ); ?>" /></a>
-          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-          <?php the_excerpt(); ?>
-          
-        </article>
-      
-          <?php wp_reset_postdata(); // IMPORTANT - reset the $post object so the rest of the page works correctly ?>
-      
-          <?php endif; ?> 
-      
-          <?php endwhile; ?>
+<section class="home hero">
+  <div class="background" style="background: linear-gradient(rgba(0, 0, 0, 0.40), rgba(0, 0, 0, 0.40)), url(' <?php bloginfo('template_directory'); ?>/img/placeholder.jpg') center center no-repeat; background-size: cover;"></div>
+  <div class="float">
+    <div class="container">
+      <div class="content eight columns">
+      <h1>Creating effective mental health interventions for people</h1>
+      <a href="#" class="button secondary">More Info</a> 
       </div>
-      <?php endif; ?>
-      
     </div>
   </div>
 </section>
 
-<?php // get_template_part( 'inc/home_latest' ); ?>
+<section class="home_about">
+  <div class="container">
+    <div class="twelve columns">
+      <h2>About</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
+  </div>
+</section>
 
+<section class="home_resources">
+  <div class="resources_bg" style="background: url('<?php bloginfo('template_directory'); ?>/img/placeholder.jpg') center center no-repeat; background-size: cover;"></div>
+  <div class="resources_content">
+    <div class="content">
+      <h3>Resources</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <a href="#" class="button secondary">Read More</a>
+    </div>
+  </div>
+ 
+</section>
 <?php endwhile; // end of the loop. ?>
 <?php get_footer(); ?>
