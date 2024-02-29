c81ddb5a21e9b988edb18ead90c005624d81bf19
Tom Brooks <tom@olospo.co.uk>
2024-02-29 13:38:19 +0000

    Template updates

--- templates/home.php
+++ templates/home.php
@@ -37,7 +37,7 @@ while ( have_posts() ) : the_post(); ?>
     <div class="content">
       <h3>Resources</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <a href="#" class="button secondary">Read More</a>
+      <a href="#" class="button primary">Read More</a>
     </div>
   </div>
  
