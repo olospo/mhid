fa33abc8e106c1f6aff83127ae6d84a737fee03b
Tom Brooks <tom@olospo.co.uk>
2024-03-14 15:40:44 +0000

    Fix Javascript

--- js/functions.js
+++ js/functions.js
@@ -70,6 +70,31 @@ $(function(){
 });
 
 // ------------------------------------------------------------
+// Animation Javascript
+// ------------------------------------------------------------
+
+var componentVisible = (function ($) {
+  
+  var $components = $('section, .step');
+
+  var componentsWaypoints = $components.waypoint({
+    handler: function() {
+      $(this.element).addClass("visible");
+    },
+    offset: '80%'
+  });
+
+})(jQuery);
+
+// Mobile Menu 
+$("li.menu-item-has-children > a").after("<div class='sub-toggle'></div>");
+
+$(".sub-toggle").click(function() {
+  $(this).siblings('ul').toggle();
+  $(this).toggleClass("open");
+});
+
+// ------------------------------------------------------------
 // Accordian
 // ------------------------------------------------------------
 
@@ -131,28 +156,3 @@ $(window).on('scroll', checkInView); // Add the scroll event handler
 checkInView(); // Check if the section is in view on page load
 
 var counter = 0;
-  
-// ------------------------------------------------------------
-// Animation Javascript
-// ------------------------------------------------------------
-
-var componentVisible = (function ($) {
-  
-  var $components = $('section, .step');
-
-  var componentsWaypoints = $components.waypoint({
-    handler: function() {
-      $(this.element).addClass("visible");
-    },
-    offset: '80%'
-  });
-
-})(jQuery);
-
-// Mobile Menu 
-$("li.menu-item-has-children > a").after("<div class='sub-toggle'></div>");
-
-$(".sub-toggle").click(function() {
-  $(this).siblings('ul').toggle();
-  $(this).toggleClass("open");
-});
