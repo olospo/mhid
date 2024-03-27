68757cbe7fadce8e0b4f723fd04cbe950c4a31d6
Tom Brooks <tom@olospo.co.uk>
2024-03-27 12:30:27 +0000

    Sidebar fixes

--- flex/content.php
+++ flex/content.php
@@ -79,7 +79,7 @@ if( $layout == 'one' ) { ?>
 
       </div>
       <?php } ?>
-      <div>
+      <div class="page_extra">
       <h3>Sidebar</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       </div>
