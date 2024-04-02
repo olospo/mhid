eba42cd1a242bef44d2c039004e59245837ae7f4
Tom Brooks <tom@olospo.co.uk>
2024-04-02 13:14:13 +0100

    Content fix

--- flex/content.php
+++ flex/content.php
@@ -49,22 +49,21 @@ if( $layout == 'one' ) { // One Column. ?>
 <?php } if( $layout == 'sidebar-two' ) { // Two Columns + Sidebar. ?>
 <section class="content_section two_columns_sidebar">
   <div class="container">
-      <?php if( have_rows('column_one') ): while( have_rows('column_one') ): the_row(); // Content One
-        $contentOne = get_sub_field('content_one');
-      ?>
-      <div class="four columns">
-        <?php echo $contentOne; ?>
-      </div>
-      <?php endwhile; endif; ?>
-      <?php if( have_rows('column_two') ): while( have_rows('column_two') ): the_row(); // Content Two
-        $contentTwo = get_sub_field('content_two');
-      ?>
-      <div class="four columns">
-        <?php echo $contentTwo; ?>
-      </div>
-      <?php endwhile; endif; ?>
-      <?php get_template_part('inc/sidebar'); // Sidebar include. ?>
+    <?php if( have_rows('column_one') ): while( have_rows('column_one') ): the_row(); // Content One
+      $contentOne = get_sub_field('content_one');
+    ?>
+    <div class="four columns">
+      <?php echo $contentOne; ?>
     </div>
+    <?php endwhile; endif; ?>
+    <?php if( have_rows('column_two') ): while( have_rows('column_two') ): the_row(); // Content Two
+      $contentTwo = get_sub_field('content_two');
+    ?>
+    <div class="four columns">
+      <?php echo $contentTwo; ?>
+    </div>
+    <?php endwhile; endif; ?>
+    <?php get_template_part('inc/sidebar'); // Sidebar include. ?>
   </div>
 </section>
 <?php } ?>
