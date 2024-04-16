9024dea06304eae74da824958af6dacced32367c
Tom Brooks <tom@olospo.co.uk>
2024-04-16 12:13:11 +0100

    Default page sidebar

--- page.php
+++ page.php
@@ -15,37 +15,44 @@ get_header(); ?>
 
 <section class="post">
   <div class="container flex">
-    <div class="content <?php if( have_rows('add_resource')) { ?>nine columns<?php } else { ?>twelve columns<?php } ?>">
+    <div class="content eight columns">
       <div class="breadcrumbs">
         <?php if (function_exists('breadcrumbs')) breadcrumbs(); ?>
       </div>
       <?php the_content(); ?>
-    <?php if (have_rows('section_content')) { // Flexible Content ?>
-    <div class="flexible_content">
-      <?php while (have_rows('section_content')) { the_row(); ?>
-        <?php if( get_row_layout() == 'columns' ): ?>
-          <?php get_template_part( 'flexible/columns'); // Column ?>
-        <?php elseif( get_row_layout() == 'content_block' ): ?>
-          <?php get_template_part( 'flexible/content_block'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'two_columns' ): ?>
-          <?php get_template_part( 'flexible/two_columns'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'columns' ): ?>
-          <?php get_template_part( 'flexible/columns'); // Content Block ?>
-        <?php elseif( get_row_layout() == 'accordion' ): ?>
-          <?php get_template_part( 'flexible/accordion'); // Accordion Block ?>
-        <?php endif; ?>
-      <?php } ?>
-    </div>
-    <?php } // End Flexible Content?>  
     </div> 
     
-    <?php if( have_rows('add_resource')) { ?>
-    <aside class="three columns">
-      <div class="resources">
-        <?php get_template_part( 'inc/resources' ); ?>
+    <aside class="four columns">
+      <div class="page_nav">
+        <h3>In this section</h3>
+        <?php global $post;
+        $current_page_id = $post->ID; // Save the current page ID for later comparison.
+        
+        if ( $post ) : 
+          // Determine if the current page has a parent to fetch siblings accordingly.
+          $parent_id = ($post->post_parent) ? $post->post_parent : $post->ID;
+          // Setup the query arguments.
+          $args = array(
+            'post_type'     => 'page',
+            'post_parent'   => $parent_id, // Fetch pages with the same parent.
+            'post_status'   => 'publish', // Only published pages.
+            'nopaging'      => true, // Get all pages without pagination.
+            'orderby'       => 'menu_order', // Order by menu order.
+            'order'         => 'ASC', // Ascending order.
+          );
+      
+          $siblings = new WP_Query($args);
+          if ( $siblings->have_posts() ) :
+        ?>
+        <ul>
+          <?php while ( $siblings->have_posts() ) : $siblings->the_post(); ?>
+          <li <?php if($current_page_id == get_the_ID()) echo 'class="current"'; ?>><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+          <?php endwhile; wp_reset_postdata(); // Reset post data to the original global post. ?>
+        </ul>
+        <?php endif; endif; ?>
       </div>
     </aside>
-    <?php }  wp_reset_postdata(); ?>
+    <?php wp_reset_postdata(); ?>
   </div>
 </section>
 
