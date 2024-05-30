d9d8f98c5a77157d11c35cfe502742f4c75e04da
Tom Brooks <tom@olospo.co.uk>
2024-05-30 14:05:39 +0100

    Resource archive

---
+++ archive-resource.php
@@ -0,0 +1,40 @@
+<?php /* Resource Archive */
+get_header(); ?>
+
+<section class="hero single">
+  <div class="container">
+    <div class="content ten columns">
+      <h1>Resources</h1>
+    </div>
+  </div>
+</section>
+
+<section class="archive">
+  <div class="container">
+    <div class="twelve columns">
+      <div class="news_listing">
+        <?php 
+          query_posts(array( 
+            'post_type' => 'resource',
+            'showposts' => 24,
+          ));  
+        ?>
+        <div class="news_listing">
+            <?php if ( have_posts() ) : while (have_posts()) : the_post();  ?>
+              <?php get_template_part('inc/article'); ?>
+            <?php endwhile; ?>
+          </div>
+          <div class="twelve columns">
+          <?php numeric_posts_nav(); ?>
+          </div>
+          <?php else : ?>
+          <!-- No posts found -->
+          <?php endif; wp_reset_query(); ?>
+        </div>
+        
+        
+    </div>
+  </div>
+</section>
+
+<?php get_footer();  ?>
