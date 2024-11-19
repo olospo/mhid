ab57678e1069338cb7e388bff233743ae1b410db
Tom Brooks <tom@olospo.co.uk>
2024-11-19 10:47:56 +0000

    News fix

--- index.php
+++ index.php
@@ -1,35 +1,13 @@
 <?php /* Archive */
-get_header(); 
+get_header(); ?>
 
-while ( have_posts() ) : the_post(); ?>
-
-<div class="flexible_content">
-    <?php 
-    while (have_rows('section_content')): the_row(); 
-        // Check if we're at the first 'content_section' and breadcrumbs haven't been shown
-        if (get_row_layout() == 'content_section' && !$breadcrumbs_displayed):
-          get_template_part('flex/breadcrumbs');
-          $breadcrumbs_displayed = true; // Set the flag to true after displaying breadcrumbs
-        endif;
-        // Continue with your layout rendering
-        if (get_row_layout() == 'hero'): 
-          get_template_part('flex/hero'); // Hero section
-        elseif (get_row_layout() == 'content_section'): 
-          get_template_part('flex/content'); // Content section
-        elseif (get_row_layout() == 'stats'): 
-          get_template_part('flex/stats'); // Stats section
-        elseif (get_row_layout() == 'square'): 
-          get_template_part('flex/square'); // Square section
-        elseif (get_row_layout() == 'intro'): 
-          get_template_part('flex/intro'); // Intro section
-        elseif (get_row_layout() == 'accordion'): 
-          get_template_part('flex/accordion'); // Accordion section
-        elseif (get_row_layout() == 'news_section'): 
-          get_template_part('flex/news'); // Accordion section
-        endif;
-    endwhile; 
-    ?>
-</div>
+<section class="hero single">
+  <div class="container">
+    <div class="content ten columns">
+      <h1>News</h1>
+    </div>
+  </div>
+</section>
 
 <section class="archive">
   <div class="container">
@@ -48,7 +26,5 @@ while ( have_posts() ) : the_post(); ?>
     </div>
   </div>
 </section>
-
-<?php endwhile; // end of the loop. ?> 
 
 <?php get_footer();  ?>
