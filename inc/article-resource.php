d9bb1d4d00d45ad2688be88dd364474dcc25fd0a
Tom Brooks <tom@olospo.co.uk>
2025-08-21 13:42:24 +0100

    Remove events

--- inc/article-resource.php
+++ inc/article-resource.php
@@ -6,17 +6,6 @@
   </a>
   <div class="content">
     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-    
-    <?php if ( get_field('event') ): 
-      $event_date = get_field('event_date'); // format: Y-m-d
-      $start_time = get_field('event_start_time'); // format: H:i
-      $end_time = get_field('event_end_time'); // format: H:i
-    ?>
-      <span class="date"><?php echo $event_date . ' - ' . $start_time . ' to ' . $end_time; ?></span>
-    <?php else: ?>
-      <span class="date"><?php the_time("F j, Y"); ?></span>
-    <?php endif; ?>
-    
     <!-- Display categories -->
     <?php
     // Figure out which taxonomy to show on this card.
